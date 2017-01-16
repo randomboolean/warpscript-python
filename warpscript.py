0ddb99be23805f3707b200e86a43d8e946db5e82
rbool <randomboolean@gmail.com>
2017-01-16 14:33:03 +0100

    added option to specify read token as argument rather than inside warpscript code

--- warpscript.py
+++ warpscript.py
@@ -4,13 +4,18 @@ url = 'https://warp.cityzendata.net/api/v0/exec'
 
 import requests as req
 import json
+import re
 
-def post(warpscript, url=url, gzip=False, timeout=None, verifySSL=True):
+def post(warpscript, url=url, token=None, gzip=False, timeout=None, verifySSL=True):
     
     # check if warpscript is a filename
     if len(warpscript) > 4:
         if warpscript[-4:] == '.mc2':
             warpscript = open(warpscript).read()
+
+    # handling token
+    if not(token is None):
+        warpscript = re.sub(".*'TOKEN' STORE", "'" + token + "' 'TOKEN' STORE", warpscript)
     
     # posting
     headers = {'Accept-Encoding':'gzip'} if gzip else None
@@ -25,17 +30,19 @@ def post(warpscript, url=url, gzip=False, timeout=None, verifySSL=True):
 if __name__ == '__main__':
     import argparse as ap
     
-    parser = ap.ArgumentParser(description='Post warpscript code to a warp10 platform')
+    parser = ap.ArgumentParser(description='Post warpscript code to a Warp 10 platform')
     parser.add_argument('warpscript', metavar='code', nargs='+',
                      help='Warpscript code or filename ending with ".mc2"')
     parser.add_argument('--url', '-u', nargs='?', default=url,
                      help='Url of Warp10 platform. Default to https://warp.cityzendata.net/api/v0/exec')
+    parser.add_argument('--token', '-t', nargs='?', default=None,
+                     help="In the warpscript code, will overwrite start of lines ending with: 'TOKEN' STORE. Do nothing if this argument is not set")
     parser.add_argument('--gzip', '-g', action='store_true',
                      help='Use gzip encoding. Default to False')
-    parser.add_argument('--timeout', '-t', nargs='?', default=None, type=int,
+    parser.add_argument('--timeout', '-s', nargs='?', default=None, type=int,
                      help='Timeout value (in s). Default to None')
-    parser.add_argument('--verifySSL', '-s', action='store_false',
-                     help='Verify SSL or not. Default to True')
+    parser.add_argument('--dontVerifySSL', '-n', action='store_false',
+                     help='Verify SSL or not. Verify SSL per default')
     
     args = parser.parse_args()
     
