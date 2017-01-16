75ba348d3fe212cf21d6e525a6b4bb2d120eec87
rbool <randomboolean@gmail.com>
2017-01-16 14:34:58 +0100

    same changes as in previous commit but to permalink.py

--- permalink.py
+++ permalink.py
@@ -5,12 +5,16 @@ url_quantum = 'https://home.cityzendata.net/quantum/'
 
 import base64 as b64
 
-def generate(warpscript, plot=False, url=url, url_quantum=url_quantum):
+def generate(warpscript, plot=False, url=url, url_quantum=url_quantum, token=None):
     
     # check if warpscript is a filename
     if len(warpscript) > 4:
         if warpscript[-4:] == '.mc2':
             warpscript = open(warpscript).read()
+
+    # handling token
+    if not(token is None):
+        warpscript = re.sub(".*'TOKEN' STORE", "'" + token + "' 'TOKEN' STORE", warpscript)
     
     return url_quantum + ('#/plot/' if plot else '#/warpscript/') + b64.b64encode(warpscript) + '/' + b64.b64encode(url)
 
@@ -26,6 +30,8 @@ if __name__ == '__main__':
                      help='Url of quantum application. Default to ' + url_quantum)
     parser.add_argument('--plot', '-p', action='store_true',
                      help='Link directly to the plot. Default to False')
+    parser.add_argument('--token', '-t', nargs='?', default=None,
+                     help="In the warpscript code, will overwrite start of lines ending with: 'TOKEN' STORE. Do nothing if this argument is not set")
     
     args = parser.parse_args()
     warpscript = (' ').join(args.warpscript)
